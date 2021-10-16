9effb7003acf31e26486feda181ff121829aafd4
Luigi <4554967+luigia@users.noreply.github.com>
2021-10-16 19:41:50 -0400

    Add route for address search

--- legislative-awareness/src/App.js
+++ legislative-awareness/src/App.js
@@ -1,25 +1,25 @@
-import './App.css'
-import icon from "./icon.jpg"
+import "./App.css";
+import icon from "./icon.jpg";
 
-import React, { Component } from "react"
-import { BrowserRouter as Router, Switch, Route } from "react-router-dom"
+import React, { Component } from "react";
+import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
 
-import { Navigation, Footer, Home} from "./components"
-
+import { Navigation, Footer, Home, Local } from "./components";
 
 function App() {
-    return (
-        <div className="App">
-        <Router>
-            <Navigation />
-                <Switch>
-                    <Route path="/" exact component={() => <Home />}></Route>
-                    {/* <Route path="/" exact component={() => <Home />} /> */}
-                </Switch>
-            <Footer />
-        </Router>
-        </div>
-    );
+  return (
+    <div className="App">
+      <Router>
+        <Navigation />
+        <Switch>
+          <Route path="/" exact component={() => <Home />}></Route>
+          <Route path="/local-info" exact component={Local}></Route>
+          {/* <Route path="/" exact component={() => <Home />} /> */}
+        </Switch>
+        <Footer />
+      </Router>
+    </div>
+  );
 }
 
 export default App;
