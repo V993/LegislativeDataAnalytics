b345bb8b98bfb6c09b1c270e11c95c2869d18d8b
Luigi <4554967+luigia@users.noreply.github.com>
2021-10-22 21:36:09 -0400

    Fix undefined data type error
    State contains the dataset and styling

--- legislative-awareness/src/components/Bar.js
+++ legislative-awareness/src/components/Bar.js
@@ -1,5 +1,5 @@
-import React from 'react';
-import { Bar } from 'react-chartjs-2';
+import React from "react";
+import { Bar } from "react-chartjs-2";
 
 /**
  *  REQUIRED INFORMATION:
@@ -7,43 +7,42 @@ import { Bar } from 'react-chartjs-2';
  *          - DATA (we want the number of bills per representative and comittee respectively)
  *          - LABEL (in our case, this will always )
  *          - TEXT (the title of the chart)
- * 
+ *
  *  The required info is filled in with random values below for now.
  */
 
-// Example: 
+// Example:
 const state = {
-    labels: ["Representatives", "Go", "Here"],
+  labels: ["Representatives", "Go", "Here"],
 
-    datasets: [
-        {
-            label: "Actions (Bills)", // The key for the data
-            backgroundColor: 'rgb(181, 144, 225)',
-            borderColor: 'rgb(0,0,0)',
-            borderWidth: 2,
-            data: [1,2,3]
-
-        }
-    ]
-}
+  datasets: [
+    {
+      label: "Actions (Bills)", // The key for the data
+      backgroundColor: "rgb(181, 144, 225)",
+      borderColor: "rgb(0,0,0)",
+      borderWidth: 2,
+      data: [1, 2, 3],
+    },
+  ],
+};
 
 export const BarChart = ({ chartData }) => {
-    return (
-        <div>
-            <Bar
-                data = {chartData}
-                options={{
-                    title:{
-                        display:true,
-                        text: "Title Goes Here",
-                        fontSize:20,
-                    },
-                    legend:{
-                        display:true,
-                        position:'right'
-                    }
-                }}
-            />
-        </div>
-    )
-}
+  return (
+    <div>
+      <Bar
+        data={state}
+        options={{
+          title: {
+            display: true,
+            text: "Title Goes Here",
+            fontSize: 20,
+          },
+          legend: {
+            display: true,
+            position: "right",
+          },
+        }}
+      />
+    </div>
+  );
+};
