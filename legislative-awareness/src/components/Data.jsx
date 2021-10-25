12c4c0f7a3df6e18beddfdc79ca06f236239dc70
Luigi <4554967+luigia@users.noreply.github.com>
2021-10-25 14:32:30 -0400

    Added bills chart and moved data to center

--- legislative-awareness/src/components/Data.jsx
+++ legislative-awareness/src/components/Data.jsx
@@ -1,21 +1,20 @@
-import React from "react"
-import { BarChart } from "./Bar"
+import React from "react";
+import { BarChart } from "./Bar";
+import Bills from "./Bills";
 
 function Data() {
-    return (
-        <div className="data">
-            <div class="container">
-                <div class="row align-items-center my-5">
-                    <div class="col-lg-5">
-                        <h1 class="font-weight-light">
-                            Data Stuff
-                        </h1>
-                        <BarChart />
-                    </div>
-                </div>
-            </div>
+  return (
+    <div className="data">
+      <div class="container">
+        <div class="row align-items-center my-5">
+          <div class="col-lg-12">
+            <h1 class="font-weight-light">Representatives' Data</h1>
+            <Bills />
+          </div>
         </div>
-    )
+      </div>
+    </div>
+  );
 }
 
 export default Data;
