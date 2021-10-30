4e6b22f1f0df9a367c273b269ef510924ba0b0db
Allan Anderson <47725827+sarge-al@users.noreply.github.com>
2021-10-30 16:36:27 -0400

    Update Information.jsx

--- legislative-awareness/src/components/Information.jsx
+++ legislative-awareness/src/components/Information.jsx
@@ -5,37 +5,37 @@ function Information() {
         <div className="information">
             <div class="container">
                 <div class="row align-items-center my-5">
-                    <div class="col-lg-5">
                         <h1 class="font-weight-light">
                             Welcome to the Information Page!
                         </h1>
-			<p class="row align-items-left my-5"> 			
-				This product aims to make educating oneself about the goings-on in 	
-				government a <strong>click away</strong>! Whether that involves 
-				knowing who your representatives are, or what your government is
- 				up to. 
-			</p>
-                    </div>
-			 <h2 class="font-weight-light">
-			    	Transparency through Centralized Information
-			 </h2>
-			 <p class="row align-items-left my-5"> 
-			 	As a representative democracy, the U.S. is relatively transparent
-				with its legislative decision-making but this data is massive and 
-				difficult to understand without a good amount of time for research. 
-			 	That is where we come in!
-			 </p>
-		  </div>
-			<h2 class="font-weight-light">
-				Our Motivation for the Page!
-			</h2>
-			<p class="row align-items-left my-5">
-				After speaking with lobbyist for city hall about the complexity of 
-				all this readily-available data, we decided that the best way to represent 
-				it all is through <strong>data manipulation and analytics</strong>! 
-			</p>
-                </div>   
-            </div>
+                        <p class="row align-items-left my-5"> 
+                                This product aims to make educating oneself about the goings-on in 
+                                government a click away! Whether that involves
+                                knowing who your representatives are, or what your government is
+                                up to.
+                        </p>
+
+                         <h2 class="font-weight-light">
+                                Transparency through Centralized Information
+                         </h2>
+                         <p class="row align-items-left my-5">
+                                As a representative democracy, the U.S. is relatively transparent
+                                with its legislative decision-making but this data is massive and
+                                difficult to understand without a good amount of time for research.
+                                That is where we come in!
+                         </p>
+                  </div>
+                        <h2 class="font-weight-light">
+                                Our Motivation for the Page!
+                        </h2>
+                        <p class="row align-items-left my-5">
+                                After speaking with lobbyist for city hall about the complexity of
+                                all this readily-available data, we decided that the best way to rep>
+                                it all is through data manipulation and analytics!
+                        </p>
+                        </div>
+                </div>
+
     )
 }
 
