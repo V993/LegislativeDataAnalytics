e4e22c6adb081b02bc08225f05325d7442cc539e
Luigi <4554967+luigia@users.noreply.github.com>
2021-11-04 12:55:57 -0400

    Add navigation to data page

--- legislative-awareness/src/components/Data.jsx
+++ legislative-awareness/src/components/Data.jsx
@@ -3,45 +3,53 @@ import { BarChart } from "./Bar";
 import Bills from "./Bills";
 import Committees from "./Committees";
 import Proximity from "./Proximity";
-import DataNavigation from "./DataNavigation";
+import Navigation from "./Navigation";
 
 export default class Data extends React.Component {
-
   constructor(props) {
     super(props);
     this.state = {
-      chart : "bills"
+      chart: "bills",
     };
-    this.showBills = e => {this.setState({chart: "bills"})};
-    this.showCommittees = e => {this.setState({chart: "committees"})};
-    this.showProximity = e => {this.setState({chart: "proximity"})};
+    this.showBills = (e) => this.setState({ chart: "bills" });
+    this.showCommittees = (e) => this.setState({ chart: "committees" });
+    this.showProximity = (e) => this.setState({ chart: "proximity" });
   }
 
   render() {
     return (
-      <div className="data">
-        <div class="container">
-          <div class="row align-items-center my-5">
-            <div class="col-lg-12">
-              <h1 class="font-weight-light">Legislative Data</h1>
-              <div>
-                <button class="data-select-button" onClick={this.showBills}>
-                  Bills Sponsored by a Representative
-                </button>
-                <button class="data-select-button" onClick={this.showCommittees}>
-                  Bills Considered by a Committee
-                </button>
-                <button class="data-select-button" onClick={this.showProximity}>
-                  Voting Proximity Between Representatives
-                </button>
+      <>
+        <Navigation />
+        <div className="data">
+          <div class="container">
+            <div class="row align-items-center my-5">
+              <div class="col-lg-12">
+                <h1 class="font-weight-light">Legislative Data</h1>
+                <div>
+                  <button class="data-select-button" onClick={this.showBills}>
+                    Bills Sponsored by a Representative
+                  </button>
+                  <button
+                    class="data-select-button"
+                    onClick={this.showCommittees}
+                  >
+                    Bills Considered by a Committee
+                  </button>
+                  <button
+                    class="data-select-button"
+                    onClick={this.showProximity}
+                  >
+                    Voting Proximity Between Representatives
+                  </button>
+                </div>
+                {this.state.chart == "bills" ? <Bills /> : <div />}
+                {this.state.chart == "committees" ? <Committees /> : <div />}
+                {this.state.chart == "proximity" ? <Proximity /> : <div />}
               </div>
-              {this.state.chart == "bills" ? <Bills /> : <div/>}
-              {this.state.chart == "committees" ? <Committees /> : <div/>}
-              {this.state.chart == "proximity" ? <Proximity /> : <div/>}
             </div>
           </div>
         </div>
-      </div>
+      </>
     );
   }
 }
