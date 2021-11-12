3b43dcfc78039fc488243dcb8ee6ea977a85fe11
Luigi <luigiagcaoili@gmail.com>
2021-11-12 13:19:30 -0500

    Format files with Prettier

--- legislative-awareness/src/components/Header.js
+++ legislative-awareness/src/components/Header.js
@@ -1,36 +1,44 @@
-import { Link } from "react-router-dom"
-import "./style.css"
+import { Link } from "react-router-dom";
+import "./style.css";
 
 export default function Header() {
-    
-
-    
-
-
-    return(
-        <div>
-            <header>
-                <Link to="/" className="link">
-                    <h1>Legislative Awareness</h1>
-                </Link>
-            </header>            
-            <div className="split">
-                <div className="textBlock">
-                    <h3 className="underline">What we are:</h3>
-                Legislative Awareness is a project dedicated to making readily-available government 
-                resources more digestible to the average consituent. It is a tool, compiling multiple
-                sources of government data in one, easy to use, comprehensive web application. 
-                Below are options to view data, find your local representatives, and learn about the
-                way government is structured in New York State. 
-                </div>
-                <div className="textBlock">
-                    <h3 className="underline">What comes next:</h3>
-                <li>More data is on the way. We plan to add NY State Senate and NY State Assembly databases. </li>
-                <li>More charts. Additional charts based on representative information.</li>
-                <li>Deeper dives into NY databases. We plan on plotting more information and increasing the capabilities of users to interact with the data we have in our database.</li>
-                <li>Interaction with representatives and discourse, as well as user accounts.</li>
-                </div>
-            </div>
+  return (
+    <div>
+      <header>
+        <Link to="/" className="link">
+          <h1>Legislative Awareness</h1>
+        </Link>
+      </header>
+      <div className="split">
+        <div className="textBlock">
+          <h3 className="underline">What we are:</h3>
+          Legislative Awareness is a project dedicated to making
+          readily-available government resources more digestible to the average
+          consituent. It is a tool, compiling multiple sources of government
+          data in one, easy to use, comprehensive web application. Below are
+          options to view data, find your local representatives, and learn about
+          the way government is structured in New York State.
         </div>
-    )
+        <div className="textBlock">
+          <h3 className="underline">What comes next:</h3>
+          <li>
+            More data is on the way. We plan to add NY State Senate and NY State
+            Assembly databases.{" "}
+          </li>
+          <li>
+            More charts. Additional charts based on representative information.
+          </li>
+          <li>
+            Deeper dives into NY databases. We plan on plotting more information
+            and increasing the capabilities of users to interact with the data
+            we have in our database.
+          </li>
+          <li>
+            Interaction with representatives and discourse, as well as user
+            accounts.
+          </li>
+        </div>
+      </div>
+    </div>
+  );
 }
--- legislative-awareness/src/components/index.js
+++ legislative-awareness/src/components/index.js
@@ -1,11 +1,9 @@
-
 export { default as Navigation } from "./Navigation";
 export { default as Footer } from "./Footer";
 export { default as Home } from "./Home";
 export { default as Local } from "./Local";
 export { default as Data } from "./Data";
 export { default as Information } from "./Information";
-
 
 // export { default as About } from "./About";
 // export { default as Contact } from "./Contact";
