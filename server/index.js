619e379488d31662f78e0cc959c26d982e58d266
ZhongweiL <zhongweili95@gmail.com>
2021-11-18 22:56:30 -0500

    debug assembly & senate info routes

--- server/index.js
+++ server/index.js
@@ -106,7 +106,7 @@ app.get("/info-apis/council-member-info", async function(req, res) {
 app.get("/info-apis/senate-info", async function(req, res) {
     const name = req.query.name;
     try {
-        const senateInfo = await pool.query("SELECT * FROM senate WHERE Name = $1", [name]);
+        const senateInfo = await pool.query("SELECT * FROM senate WHERE fullName = $1", [name]);
         res.json(senateInfo.rows[0]);
     } catch (error) {
         console.error(error.message)
@@ -117,7 +117,7 @@ app.get("/info-apis/senate-info", async function(req, res) {
 app.get("/info-apis/assembly-info", async function(req, res) {
     const name = req.query.name;
     try {
-        const assemblyInfo = await pool.query("SELECT * FROM assembly WHERE Name = $1", [name]);
+        const assemblyInfo = await pool.query("SELECT * FROM assembly WHERE fullName = $1", [name]);
         res.json(assemblyInfo.rows[0]);
     } catch (error) {
         console.error(error.message)
