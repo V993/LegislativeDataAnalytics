545e36bbc2fd87c352081b114a4e326034225400
Allan Anderson <47725827+sarge-al@users.noreply.github.com>
2021-12-10 10:47:31 -0500

    Update DataNavbar.jsx

--- legislative-awareness/src/components/bodies/DataNavbar.jsx
+++ legislative-awareness/src/components/bodies/DataNavbar.jsx
@@ -1,19 +1,3 @@
-
-import React from "react";
-import { Link, withRouter } from "react-router-dom";
-
-function Navigation(props) {
-  return (
-    <div>
-     
-    </div>
-  );
-}
-
-var NavigationBar = withRouter(Navigation)
-
-export default NavigationBar;
-
 import React from "react";
 import { Link, withRouter } from "react-router-dom";
 
