e68c2f62a96b4c13782f3463c651d947c55eb746
Allan Anderson <47725827+sarge-al@users.noreply.github.com>
2021-12-10 10:45:39 -0500

    Update NotFound.jsx

--- legislative-awareness/src/components/NotFound.jsx
+++ legislative-awareness/src/components/NotFound.jsx
@@ -1,19 +1,3 @@
-
-import { Link } from 'react-router-dom'
-
-export const NotFound = () => {
-    return (
-        <html id="center">
-            <img src="404NotFound.jpg" alt="even the pic was not found yo"/>
-
-            <div id="smolButton">
-                <Link to="/" className="smolButton">
-                    Back to Home
-                </Link>
-            </div>
-        </html>
-    )
-
 import { Link } from 'react-router-dom'
 
 export const NotFound = () => {
