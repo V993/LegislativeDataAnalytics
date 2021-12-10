65a5df6aadcd2103e7d7a1404380cfd6e2cc829a
Allan Anderson <47725827+sarge-al@users.noreply.github.com>
2021-12-10 10:54:35 -0500

    Update Senate.jsx

--- legislative-awareness/src/components/bodies/Senate.jsx
+++ legislative-awareness/src/components/bodies/Senate.jsx
@@ -8,13 +8,8 @@ import { Link } from 'react-router-dom';
 import {AiFillCaretDown} from 'react-icons/ai';
 
 // Dependencies:
-
 import Bills from "./chartGenerators/senate_bills";
 import Committees from "./chartGenerators/senate_committees";
-
-import Bills from "./chartGenerators/Bills";
-import Committees from "./chartGenerators/Committees";
-
 import Proximity from "./chartGenerators/Proximity";
 import Navigation from "./DataNavbar";
 import "./layout.css";
@@ -27,7 +22,6 @@ const ColorButton = styled(Button)(({ theme }) => ({
   backgroundImage:
     "linear-gradient(rgba(138, 182, 169, 0.5), rgba(255, 255, 255, 0))",
 }));
-
 
 //const img = require("./assets/imgs");
 
@@ -95,11 +89,6 @@ export default class Data extends React.Component {
              {/*this.state.member.imgname*/}       
               
                 <p>District {this.state.member.districtcode}</p>
-              <h4 className="rep-name">{this.state.member.name}</h4>
-              <small className="rep-details">
-                <p>{this.state.member.politicalparty}</p>
-                <p>District {this.state.member.district}</p>
-                <p>{this.state.member.borough}</p>
               </small>
             </div>
           )}
